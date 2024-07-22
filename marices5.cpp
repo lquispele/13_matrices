7e2afcc1aa36c543940bd744ba5bf878595643e9
lquispele <lquispele@unjbg.edu.pe>
2024-07-22 00:05:10 -0500

    Agregando el problema 5 de matrices

---
+++ marices5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Ingresen la cantidad de partidos politicos: "; cin>>n;
+    cout<<"Registre las ciudades: "; cin>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>matriz[i][j];
+        }
+    }
+    return EXIT_SUCCESS;
+}
